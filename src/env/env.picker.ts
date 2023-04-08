f40413c9efa40a6d16fcfbae0bed9b1b64c0029b
allohamora <herman30698@gmail.com>
2023-04-08 09:48:58 +0300

    feat: add mapIfExist to env-picker

--- src/env/env.picker.ts
+++ src/env/env.picker.ts
@@ -2,6 +2,14 @@ export type WrappedInEnvPickers<T> = {
   [K in keyof T]: EnvPicker<T[K]>;
 };
 
+type MoveOptional<T, R> = T extends null
+  ? T extends undefined
+    ? R | null | undefined
+    : R | null
+  : T extends undefined
+  ? R | undefined
+  : R;
+
 export const wrapInEnvPickers = <C extends Record<string, string | undefined>>(config: C, nodeEnv: string) => {
   return Object.keys(config).reduce((result, key) => {
     return { ...result, [key]: new EnvPicker(config[key], nodeEnv) };
@@ -27,6 +35,14 @@ export class EnvPicker<S> {
     this.state = mapper(this.state) as unknown as S;
 
     return this as unknown as EnvPicker<R>;
+  }
+
+  public mapIfExist<R>(mapper: (state: NonNullable<S>) => R) {
+    if (this.state !== null && this.state !== undefined) {
+      this.state = mapper(this.state as NonNullable<S>) as unknown as S;
+    }
+
+    return this as unknown as EnvPicker<MoveOptional<S, R>>;
   }
 
   public value() {
